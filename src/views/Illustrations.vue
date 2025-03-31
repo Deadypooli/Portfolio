480c5db273c1c2a00ec4b031e0c626adefe26065
Neofy <96073814+Deadypooli@users.noreply.github.com>
2025-03-31 15:52:31 +0200

    sliding illustrations

--- src/views/Illustrations.vue
+++ src/views/Illustrations.vue
@@ -1,21 +1,27 @@
 <template>
     <Header />
     <div class="illustrations-container">
-        <div class="illustrations-items">
+        <button
+        class="illustration-btn"
+        v-on:click="slide(-1)">
+        left
+        </button>
+        <div
+        ref="illustrationsItems"
+        class="illustrations-items">
+
+            <img 
+            class="item"
+            v-for="key in imgArray"
+            :src="key"
+            :key="key.url">
 
         </div>
-        <div class="item">
-
-        </div>
-        <div class="item">
-
-        </div>
-        <div class="item">
-
-        </div>
-        <div class="item">
-
-        </div>
+        <button
+        class="illustration-btn"
+        v-on:click="slide(+1)">
+            right
+        </button>
     </div>
 </template>
 
@@ -24,5 +30,29 @@ import Header from '@/components/Header.vue';
 
 export default {
     components: { Header },
+
+    name: 'Gallery',
+	data () {
+		return {
+            direction: +1,
+            count: 0,
+			imgArray: [
+				require ('@/img/fir.png'),
+				require ('@/img/lorikeet.png'),
+				require ('@/img/penguin.png'),
+				require ('@/img/red-spider.png'),
+				require ('@/img/Rose.png'),
+			]
+		}
+	},
+    methods: {
+		slide (direction) {
+            console.log('in');
+            
+            this.count = this.count + (direction * 100);
+            
+            this.$refs.illustrationsItems.style.transform = 'translateX(' + this.count + '%)';
+        }
+	}
 }
 </script>
