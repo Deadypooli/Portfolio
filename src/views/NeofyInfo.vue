2bd7950a2be9ba7ee3f64d2f7264001d39d050ce
Neofy <96073814+Deadypooli@users.noreply.github.com>
2025-04-13 16:16:12 +0200

    format about me text

--- src/views/NeofyInfo.vue
+++ src/views/NeofyInfo.vue
@@ -1,18 +1,30 @@
 <template>
     <Header />
     <div class="info-text">
-        Hello! I'm a 25-year-old freelance artist driven by a lifelong passion for visual storytelling.
-        My creative journey spans 2D illustration and concept art, as well as 3D modeling and animation.
-        I thrive on bringing imaginative concepts to life through vibrant visuals and dynamic motion.
-        I'm currently immersed in the exciting process of creating my own short animated film using Blender,
-        a project that allows me to fully explore my skills and vision.
-        My experience also includes character modeling and animation for the Steam game 'Banana Drama',
-        where I contributed to crafting engaging and memorable characters.
-        While I've always been drawn to art, the past few years have seen it evolve from a hobby to my central focus.
-        I'm eager to collaborate on diverse projects, offering my skills for contracts and commissions.
-        I'm particularly interested in opportunities that allow me to contribute my unique style and technical abilities to bring creative visions to fruition.
-        I'm excited to connect and explore how my artistic skills can serve your 
-        project.
+        <h1 class="headline">Hello!</h1>
+        <p>I'm a 25-year-old freelance artist driven by a lifelong passion for visual storytelling.</p>
+
+        <p>My creative journey spans 2D illustration and concept art, as well as 3D modeling and animation.</p>
+
+        <p>I thrive on bringing imaginative concepts to life through vibrant visuals and dynamic motion.</p>
+
+        <p>
+            I'm currently immersed in the exciting process of creating my own short animated film using Blender,
+            a project that allows me to fully explore my skills and vision.
+        </p>
+
+        <p>
+            My experience also includes character modeling and animation for the Steam game 'Banana Drama',
+            where I contributed to crafting engaging and memorable characters.
+        </p>
+
+        <p>While I've always been drawn to art, the past few years have seen it evolve from a hobby to my central focus.</p>
+
+        <p>I'm eager to collaborate on diverse projects, offering my skills for contracts and commissions.</p>
+
+        <p>I'm particularly interested in opportunities that allow me to contribute my unique style and technical abilities to bring creative visions to fruition.</p>
+
+        <p>I'm excited to connect and explore how my artistic skills can serve your project.</p>
     </div>
 </template>
 
