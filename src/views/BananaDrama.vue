43b08210571c51cce00e3ed2aa32c7db8d04b623
Neofy <96073814+Deadypooli@users.noreply.github.com>
2025-09-20 19:48:00 +0200

    finishing banana drama pc version

--- src/views/BananaDrama.vue
+++ src/views/BananaDrama.vue
@@ -1,24 +1,38 @@
 <template>
     <Header />
     <div class="banana-hero">
-        <div class="banana-header">
-            <h1 class="banana">
-                Banana
-            </h1>
-            <h1 class="drama">
-                Drama
-            </h1>
-        </div>
-        <img
+        <div class="hero-container">
+
+            <div class="banana-header">
+                <h1 class="banana">
+                    Banana
+                </h1>
+                <h1 class="drama">
+                    Drama
+                </h1>
+            </div>
+            <img
             class="banana-gif" 
-            :src="animalsArray[0]">
-        <img
+            :src="cowDance">
+            <img
             class="banana-peek" 
-            :src="animalsArray[1]">
-        <!-- Banana Drama is a RTS game on steam and the first game I got to work on. It was a lot of fun for me. -->
-        <!-- I really liked being creative and design fun animal characters, that players can actually play. -->
+            :src="tigerPeek">
+            <!-- Banana Drama is a RTS game on steam and the first game I got to work on. It was a lot of fun for me. -->
+            <!-- I really liked being creative and design fun animal characters, that players can actually play. -->
+        </div>
     </div>
     <div class="banana-container">
+        <div 
+        v-for="(animal, index) in animalsArray"
+        :key="animal"
+        class="banana-turn">
+            <div class="banana-text">
+                <h2>
+                    {{ namesArray[index] }}
+                </h2>
+            </div>
+            <img :src="animal">
+        </div>
     </div>
 </template>
 
@@ -30,11 +44,25 @@
         name: 'BananaDrama',
         data() {
             return {
-                animalsArray: [
-                    require('@/img/projects/cow.gif'),
-                    require('@/img/projects/tiger-peek.png'),
+                cowDance: require('@/img/bananaDrama/cow.gif'),
+                tigerPeek: require('@/img/bananaDrama/tiger-peek.png'),
+                animalsArray: [],
+                namesArray: [
+                    'The Cockatoo',
+                    'The Cow',
+                    'The Giraffe',
+                    'The Lion',
+                    'The Tiger',
                 ]
             };
         },
+        mounted () {
+            this.importAll(require.context('@/img/bananaDrama/turns/', true));
+        },
+        methods: {
+            importAll(r) {
+                r.keys().forEach(key => (this.animalsArray.push(r(key))));  
+            },
+        }
     };
 </script>
