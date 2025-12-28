5d9eb61e5e878d7d3f4bba4a7091ae8a489be5dc
Neofy <96073814+Deadypooli@users.noreply.github.com>
2025-12-28 12:02:03 +0100

    image size and new node changes

---
+++ src/views/ConceptArt.vue
@@ -0,0 +1,35 @@
+<template>
+    <Header />
+    <main>        
+        <div
+        class="concept-container">
+                <img 
+                class="concept-img"
+                v-for="key in imgArray"
+                :src="key"
+                :key="key">
+        </div>
+    </main>
+</template>
+
+<script>
+    import Header from '@/components/Header.vue';
+
+    export default {
+        components: { Header },
+        name: 'ConceptArt',
+        data () {
+		return {
+			imgArray: [],
+            }
+        },
+        mounted () {
+            this.importAll(require.context('@/img/concepts/', true));
+        },
+        methods: {
+            importAll(r) {
+                r.keys().forEach(key => (this.imgArray.push(r(key))));
+            },
+        }
+    };
+</script>
---
+++ src/views/Illustrations.vue
@@ -0,0 +1,90 @@
+<template>
+    <Header />
+    <Display
+	:selectedImage="selectedImage"
+	v-if="clicked"
+	@clicked="toggleModal(key)"/>
+    <div class="illustrations-container">
+        <div class="illustrations-carousel">
+
+            <div
+                ref="carouselItems"
+                class="carousel-items">
+            
+                <img 
+                class="image"
+                v-for="key in imgArray"
+                :src="key"
+                ref="image"
+                :key="key"
+                @click="toggleModal(key)">
+            
+            </div>
+            <div class="carousel-controls">
+                <button
+                class="illustration-btn"
+                :class="{hidden: !(carouselPosition < 0)}"
+                v-on:click="slide(+1)">
+                <img src="../logo/angle-left.svg" class="illustration-svg">
+                </button>
+                <button
+                class="illustration-btn"
+                :class="{hidden: !(-carouselPosition < (imgArray.length - 1))}"
+                v-on:click="slide(-1)">
+                <img src="../logo/angle-right.svg" class="illustration-svg">
+            </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue';
+import Display from '@/components/Display.vue';
+
+export default {
+    components: { Header, Display },
+
+    name: 'Illustrations',
+	data () {
+		return {
+            selectedImage: null,
+            clicked: false,
+            direction: +1,
+            carouselPosition: 0,
+            count: 0,
+            imgWidth: 0,
+			imgArray: []
+		}
+	},
+    mounted() {
+        this.importAll(require.context('@/img/illustrations/', true));
+        setTimeout(() => {
+            for (var key in this.imgArray) {
+                this.imgWidth = this.$refs['image'][key].width;
+            }
+        }, 1);
+    },
+    methods: {
+        importAll(r) {
+                r.keys().forEach(key => (this.imgArray.push(r(key))));
+        },
+		slide (direction) {
+            if (direction == -1) {
+                this.count = this.count + (direction * (this.$refs['image'][-this.carouselPosition].width + 40));
+                
+            } else {
+                this.count = this.count + (direction * (this.$refs['image'][-(this.carouselPosition + 1)].width + 40));
+                
+            }
+            this.$refs.carouselItems.style.transform = 'translateX(' + this.count + 'px)';
+            
+            this.carouselPosition = this.carouselPosition + direction;
+        },
+        toggleModal (key) {			
+			this.selectedImage = key;
+			this.clicked = !this.clicked;
+		}
+	}
+}
+</script>
