fd068eb30b7998903da349266f3ac058a8234d19
Neofy <96073814+Deadypooli@users.noreply.github.com>
2026-01-01 15:02:25 +0100

    remove unused stuff

--- src/views/BananaDrama.vue
+++ src/views/BananaDrama.vue
@@ -29,43 +29,6 @@
                 Your browser does not support the video tag.
             </video>
         </div>
-    <!-- <div class="banana-hero-container">
-        <div class="banana-hero">
-            <div class="banana-hero-header">
-                <h1>
-                    Banana
-                </h1>
-                <h1>
-                    Drama
-                </h1>
-            </div>
-            <img
-            class="banana-hero-cow" 
-            :src="cowDance">
-            <img
-            class="banana-hero-tiger" 
-            :src="tigerPeek">
-        </div>
-    </div>
-    <div class="banana-container">
-        <div class="banana-description">
-            Banana Drama is a free and independent RTS game.
-            It offers a singleplayer and online multiplayer experience.
-            <br>
-            It is the first game I got to work on.
-        </div>
-        <div 
-        v-for="(animal, index) in animalsArray"
-        :key="animal"
-        class="banana-turn">
-            <div class="banana-turn-header">
-                <h2>
-                    {{ namesArray[index] }}
-                </h2>
-            </div>
-            <img :src="animal">
-        </div>
-    </div> -->
 </template>
 
 <script>
@@ -78,9 +41,6 @@
             return {
                 heroImg: require('@/img/bananaDrama/Banana-hero.webp'),
                 vid: require('@/img/bananaDrama/tiger-cleaning.mp4'),
-                // cowDance: require('@/img/bananaDrama/cow.gif'),
-                // tigerPeek: require('@/img/bananaDrama/tiger-peek.png'),
-                animalsArray: [],
                 imgArray: [
                     [
                         require('@/img/bananaDrama/cockatoo-front.webp'),
@@ -103,22 +63,7 @@
                         require('@/img/bananaDrama/tiger-side.webp')
                     ],
                 ]
-                // namesArray: [
-                //     'The Cockatoo',
-                //     'The Cow',
-                //     'The Giraffe',
-                //     'The Lion',
-                //     'The Tiger',
-                // ]
             };
         },
-        mounted () {
-            this.importAll(require.context('@/img/bananaDrama/turns/', true));
-        },
-        methods: {
-            importAll(r) {
-                r.keys().forEach(key => (this.animalsArray.push(r(key))));  
-            },
-        }
     };
 </script>
--- src/views/CarProject.vue
+++ src/views/CarProject.vue
@@ -5,25 +5,6 @@
   <img :src="imgSide" class="car-image">
   <img :src="imgBack" class="car-image">
   <img :src="imgTop" class="car-image">
-  <!-- <div class="video-container">
-    <div class="video-text">
-      <h1>
-        SOON
-      </h1>
-    </div>
-    <video class="hero-video" autoplay muted loop playsinline>
-      <source :src="vid" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
-  </div>
-  <div class="car-container">
-    <div class="car-text">
-      <h2>
-        Arriving soon
-      </h2>
-    </div>
-    <img :src="img">
-  </div> -->
 </template>
 
 <script>
@@ -36,7 +17,6 @@ export default {
   },
   data() {
     return {
-      // vid: require('@/img/car/0000-1410.mp4'),
       img: require('@/img/car/Car.webp'),
       imgFront: require('@/img/car/Car-front.webp'),
       imgBack: require('@/img/car/Car-back.webp'),
